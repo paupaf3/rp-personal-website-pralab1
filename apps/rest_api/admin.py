98c847194638f563495413d7374700020b006895
Pau Agusti - dev <pau.paf.3@gmail.com>
2020-02-25 11:41:49 +0100

    admin.py corrected

--- apps/rest_api/admin.py
+++ apps/rest_api/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-from . import models
+from apps.rest_api import models
 
-# Register your models here.
-
-#Newest Way -------------------------------------------
+# Way 1: using a decorator
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+	pass
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+	pass
 
-
-#Old Way ----------------------------------------------
+# Way 2: using the register function
 class CommentAdmin(admin.ModelAdmin):
-    pass
+	pass
 
-admin.site.register(models.Comment. CommentAdmin)
+admin.site.register(models.Comment, CommentAdmin)
+
+# The ModelAdmin class is the representation of a model in the admin interface.
+# It has several options for dealing with customizing the interface, for example
+# showing only a subset of available fields, modifying their order, or grouping
+# them into rows. Have a look at: https://docs.djangoproject.com/en/3.0/ref/contrib/admin/
