d3381a89726863a88f012de958be6e7e2ed5e108
Pau Agusti - dev <pau.paf.3@gmail.com>
2020-02-25 11:34:02 +0100

    rest-api working.

--- apps/rest_api/admin.py
+++ apps/rest_api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+#Newest Way -------------------------------------------
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+#Old Way ----------------------------------------------
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Comment. CommentAdmin)
